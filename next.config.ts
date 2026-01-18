e66180a234fe166610bab215f6f74181f540fb8e
Enzo CORATELLA <ecoratella13.pro@gmail.com>
2026-01-18 03:03:24 +0100

    refactor package.json

--- next.config.ts
+++ next.config.ts
@@ -1,6 +1,7 @@
 import type { NextConfig } from "next";
 
-const repo = process.env.NEXT_PUBLIC_BASE_PATH || `/${process.env.NEXT_PUBLIC_REPO || ""}`;
+const {NEXT_PUBLIC_BASE_PATH} = process.env
+const repo = NEXT_PUBLIC_BASE_PATH || ""
 
 const nextConfig: NextConfig = {
   output: "export",
